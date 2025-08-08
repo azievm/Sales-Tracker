4eb3e5b0b89b2970ca858d11420853c815a587d2
Majid Aziev <majidaziev007@yandex.ru>
2025-08-08 15:33:55 +0300

    Done
    Add smart contracts

--- contracts/AlbumTracker.sol
+++ contracts/AlbumTracker.sol
@@ -21,16 +21,39 @@ contract AlbumTracker {
   mapping(uint => AlbumProduct) public albums;
   uint currentIndex;
 
+  event AlbumStateChanged(uint _albumIndex, 
+  uint _stateNum, address _albumAddress);
+
   function createAlbum(uint _price, string memory _title) public onlyOwner {
-    Album newAlbum = new Album(_price, _title);
+    Album newAlbum = new Album(_price, _title, currentIndex, this);
 
     albums[currentIndex].album = newAlbum;
     albums[currentIndex].state = AlbumState.Created;
     albums[currentIndex].price = _price;
     albums[currentIndex].title = _title;
 
+    emit AlbumStateChanged(currentIndex, 
+      uint(albums[currentIndex].state), 
+      address(newAlbum));
+
     currentIndex++;
   }
 
+  function triggerPayment(uint _index) public payable {
+    require(albums[_index].state == AlbumState.Created, "This album is already purchased!");
+    require(albums[_index].price == msg.value, "We accept only full payments!");
+    
+    albums[_index].state = AlbumState.Paid;
+    
+    emit AlbumStateChanged(_index, uint(albums[_index].state), address(albums[_index].album));
+  }
+
+  function triggerDelivery(uint _index) public onlyOwner {
+    require(albums[_index].state == AlbumState.Paid, "This album is not paid for!");
+    
+    albums[_index].state = AlbumState.Delivered;
+
+    emit AlbumStateChanged(_index, uint(albums[_index].state), address(albums[_index].album));
+  }
 
 }
